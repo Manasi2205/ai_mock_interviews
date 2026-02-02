e321d54bb99dbc779d355186a5577ef24d390869
Manasi <manasibusam04@gmail.com>
2026-02-02 20:12:36 +0530

    tailwind config edited again

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  darkMode: ["class"],
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
+        success: {
+          100: "#49de50",
+          200: "#42c748",
+        },
+        destructive: {
+          100: "#f75353",
+          200: "#c44141",
+        },
         primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
+          100: "#dddfff",
+          200: "#cac5fe",
+        },
+        light: {
+          100: "#d6e0ff",
+          400: "#6870a6",
+          600: "#4f557d",
+          800: "#24273a",
+        },
+        dark: {
+          100: "#020408",
+          200: "#27282f",
+          300: "#242633",
         },
       },
     },
   },
   plugins: [],
 }
-
